c60fb6d29d25552f28d42efc365667096f1169bd
sourabhbagrecha <sourabhbagrecha@gmail.com>
2025-08-25 04:07:44 +0530

    Removed dist

--- server/dist/routes/health.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-const router = Router();
-// Get all orders (admin gets all, user gets their own)
-router.get("/", async (_, res, __) => {
-    res.status(200).json({ status: "ok" });
-});
-export const orderRoutes = router;
