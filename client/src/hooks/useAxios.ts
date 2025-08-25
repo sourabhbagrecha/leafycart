24a338c47fce1be510da1e99053e9d52324eb57d
sourabhbagrecha <sourabhbagrecha@gmail.com>
2025-08-25 14:47:14 +0530

    fixed deploy config

--- client/src/hooks/useAxios.ts
+++ client/src/hooks/useAxios.ts
@@ -7,7 +7,8 @@ import axios, {
 import { useAuth } from "./useAuth";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? "" : "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "",
 });
 
 export function useAxios(): AxiosInstance {
